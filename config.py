6cd18c56534cb80cf0b444efdede44bc63de975a
hjpwyb <122347230+hjpwyb@users.noreply.github.com>
2024-11-14 14:17:46 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ source_urls = [
 
     "https://raw.githubusercontent.com/xiongjian83/iptv/main/speedtest/zubo.txt",
     "https://raw.githubusercontent.com/xiongjian83/iptv/main/speedtest/zubo_fofa.txt",
-    "https://raw.githubusercontent.com/xiongjian83/zubo/main/speedtest/zubo.txt",
+    "https://raw.githubusercontent.com/xiongjian83/zubo/main/speedtest/zubo.txt",    
+    "https://raw.githubusercontent.com/xiongjian83/zubo/main/speedtest/zubo1.txt",
     "https://raw.githubusercontent.com/xiongjian83/zubo/main/speedtest/zubo_fofa.txt",
     "https://raw.githubusercontent.com/happymem2023/zubo/refs/heads/main/zubo.txt",
     "https://raw.githubusercontent.com/xiongjian83/iptv/main/xj.txt",
